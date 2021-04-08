36aa08255e8d11dcade95717acfece8568b17526
3ANov <3anovdev@gmail.com>
2021-04-08 12:32:54 +0300

    work version with fix

--- theme_switch/views.py
+++ theme_switch/views.py
@@ -6,11 +6,7 @@ import json
 class LightDarkSwitchView(View):
     def post(self, request, *args, **kwargs):
         post_data = json.loads(request.body.decode("utf-8"))
-        if request.session.get('dark_theme_flag', False):
-            request.session['dark_theme_flag'] = post_data['dark_theme_flag']
-        else:
-            request.session['dark_theme_flag'] = post_data['dark_theme_flag']
-
+        request.session['dark_theme_flag'] = post_data['dark_theme_flag']
         return self.get(request, *args, **kwargs)
 
 
